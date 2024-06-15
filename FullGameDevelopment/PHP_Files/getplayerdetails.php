773c81f9290d03cf767464aa851e89dfdecaedb3
Kowrisaan11 <kowrisaan2000@gmail.com>
2024-06-15 20:31:20 +0530

    committed php files

--- FullGameDevelopment/PHP_Files/getplayerdetails.php
+++ FullGameDevelopment/PHP_Files/getplayerdetails.php
@@ -1,10 +1,14 @@
 <?php
+// Allow from any origin
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
 
-// MySQL connection parameters
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "unity";
+// MySQL connection parameters (using environment variables)
+$servername = getenv('MYSQL_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$dbname = getenv('MYSQL_DATABASE');
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
--- FullGameDevelopment/PHP_Files/register.php
+++ FullGameDevelopment/PHP_Files/register.php
@@ -1,9 +1,14 @@
 <?php
-    // MySQL connection parameters
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "unity";
+    // Allow from any origin
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+    
+	// MySQL connection parameters (using environment variables)
+    $servername = getenv('MYSQL_HOST');
+    $username = getenv('MYSQL_USER');
+    $password = getenv('MYSQL_PASSWORD');
+    $dbname = getenv('MYSQL_DATABASE');
 
     // Create connection
     $con = mysqli_connect($servername, $username, $password, $dbname);
